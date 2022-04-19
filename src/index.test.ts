0942e3622546627aef82d82e1152c8501b7bf2fa
TSY <8446825+snoopy2199@users.noreply.github.com>
2022-04-19 14:15:07 +0800

    feat: use husky for lint-staged and commitlint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
 import TeHai from './index';
 
 test('Hi from TeHai', () => {
+  // eslint-disable-next-line no-console
   console.log = jest.fn();
 
   const teHai = new TeHai();
   teHai.hi();
 
+  // eslint-disable-next-line no-console
   expect(console.log).toHaveBeenCalledWith('TeHai🀄');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 class TeHai {
+  // eslint-disable-next-line class-methods-use-this
   hi(): void {
+    // eslint-disable-next-line no-console
     console.log('TeHai🀄');
   }
 }
